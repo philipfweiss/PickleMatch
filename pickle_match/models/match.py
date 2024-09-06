523d403fd4d4b373b14cdba47d8de908d02648c1
philip weiss <philip.weiss@airbnb.com>
2024-09-06 11:47:48 -0700

    change df display

--- pickle_match/models/match.py
+++ pickle_match/models/match.py
@@ -17,14 +17,15 @@ class Matches:
             yield match 
 
     def to_df(self, round_no):
+        title = f"Round {round_no} pairings:"
         team_dict = {
-            "Team 1": [],
-            "Team 2": [],
+           title: [],
+            "---": [],
         }
         for match in self.matches:
             first_pair, second_pair = match.first, match.second
-            team_dict["Team 1"].append(f"{first_pair.first.name} and {first_pair.second.name}")
-            team_dict["Team 2"].append(f"{second_pair.first.name} and {second_pair.second.name}")
+            team_dict[title].append(f"{first_pair.first.name} and {first_pair.second.name}")
+            team_dict["---"].append(f"{second_pair.first.name} and {second_pair.second.name}")
 
         dataframe = pd.DataFrame.from_dict(team_dict)
         dataframe.style.set_caption(f"Round {round_no} pairings")
