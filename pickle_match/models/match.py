7365f5281c81bc1078df56d249ee87f721ddda32
philip weiss <philip.weiss@airbnb.com>
2024-09-06 15:13:21 -0700

    second pair

--- pickle_match/models/match.py
+++ pickle_match/models/match.py
@@ -25,7 +25,7 @@ class Matches:
         for match in self.matches:
             first_pair, second_pair = match.first, match.second
             team_dict[title].append(f"(Team {first_pair.team_id}) {first_pair.first.name} and {first_pair.second.name}")
-            team_dict["---"].append(f"(Team {first_pair.team_id}) {second_pair.first.name} and {second_pair.second.name}")
+            team_dict["---"].append(f"(Team {second_pair.team_id}) {second_pair.first.name} and {second_pair.second.name}")
 
         dataframe = pd.DataFrame.from_dict(team_dict)
         dataframe.style.set_caption(f"Round {round_no} pairings")
