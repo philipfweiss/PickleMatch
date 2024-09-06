bbd960ea79c7ce02286868605d6fd36cb69b4e7a
philip weiss <philip.weiss@airbnb.com>
2024-09-06 00:45:36 -0700

    load from filename

--- pickle_match/loaders/load.py
+++ pickle_match/loaders/load.py
@@ -1,12 +1,17 @@
 import csv
 from pickle_match.models.player import Player
 
-def load_players(filename):
-    players = []
-    with open(filename, "r") as f:
-       reader = csv.reader(f)
-       for idx, (name, rating) in enumerate(reader):
-           if not idx: continue
-           players.append(Player(name=name, rating=float(rating)))
-    
-    return players
+def load_players(filename=None, players=None):
+    player_objects = []
+
+    if filename:
+        with open(filename, "r") as f:
+            reader = csv.reader(f)
+            for idx, (name, rating) in enumerate(reader):
+                if not idx: continue
+                players.append(Player(name=name, rating=float(rating)))
+    elif csv:
+        for (name, rating) in players:
+            player_objects.append(Player(name=name, rating=float(rating)))
+
+    return player_objects
